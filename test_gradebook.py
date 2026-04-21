5c3bf3906cb5f3bfd5ce15142e2f51da347f73d6
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2026-04-21 08:36:55 -0400

    ADDED PARAMETRIZE TESTS

--- test_gradebook.py
+++ test_gradebook.py
@@ -7,3 +7,9 @@ def test_letter_grade_A():
 
 def test_letter_grade_F():
     assert letter_grade(45) == "F"
+
+
+@pytest.mark.parametrize("score, expected", [(95, "A"), (45, "F")])
+def test_letter_grade(score, expected):
+    assert letter_grade(score) == expected
+    
