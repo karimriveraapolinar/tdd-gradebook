cdf5ff1568d0cc74e64bc43ea4f934d1f58c646d
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2026-04-22 07:50:43 -0400

    added typeerror to grade book

--- gradebook.py
+++ gradebook.py
@@ -1,4 +1,7 @@
 def letter_grade(score):
+    if not isinstance(score,(int, float)):
+        raise TypeError("Score must be a float or integer")
+    
     if score >= 90:
         return "A"
     elif score >= 80:
@@ -21,3 +24,4 @@ def average(scores):
 
 def curved_score(score, bonus):
     pass 
+
